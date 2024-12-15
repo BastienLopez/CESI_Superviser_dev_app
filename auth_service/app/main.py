c94f4c7746c3a780863279c5092cc5016c451d05
DAMIEN DEVOTI <damien.devoti@emotors.com>
2024-12-15 22:31:15 +0100

    features: add test auth

--- auth_service/app/main.py
+++ auth_service/app/main.py
@@ -22,83 +22,88 @@ class Users(me.Document):
         return f'<User {self.username}>'
 
 
-app = Flask(__name__)
+def get_app(config):
 
-# Configuration de MongoDB
-app.config['MONGODB_SETTINGS'] = {
-    'db': 'authdb',
-    'host': 'mongo-auth',  # Nom du service MongoDB dans Docker
-    'port': 27017,  # Port de MongoDB
-    'username': 'root',  # Nom d'utilisateur MongoDB
-    'password': 'example',  # Mot de passe MongoDB
-    'authentication_source': 'admin'  # Base de données où les infos d'authentification sont stockées
-}
+    app = Flask(__name__)
 
-# Initialisation de MongoEngine
-db = MongoEngine(app)
-
-SECRET_KEY = "secret_key"
+    # Configuration de MongoDB
+    app.config['MONGODB_SETTINGS'] = config
 
 
-@app.route("/auth/signup", methods=["POST"])
-def signup():
-    data = request.json
-    username = data.get("username")
-    email = data.get("email")
-    password = data.get("password")
+    # Initialisation de MongoEngine
+    db = MongoEngine(app)
 
-    if not username or not email or not password:
-        return jsonify({"error": "Username, email, and password are required"}), 400
-
-    # Vérifier si l'utilisateur existe déjà
-    if Users.objects(username=username):
-        return jsonify({"error": "User already exists"}), 409
-
-    user = Users(username=username, email=email)
-    user.set_password(password)  # Hachage du mot de passe
-    user.save()
-
-    return jsonify({"message": "User created successfully"}), 201
+    SECRET_KEY = "secret_key"
 
 
-@app.route("/auth/login", methods=["POST"])
-def login():
-    data = request.json
-    username = data.get("username")
-    password = data.get("password")
+    @app.route("/auth/signup", methods=["POST"])
+    def signup():
+        data = request.json
+        username = data.get("username")
+        email = data.get("email")
+        password = data.get("password")
 
-    if not username or not password:
-        return jsonify({"error": "Username and password are required"}), 400
+        if not username or not email or not password:
+            return jsonify({"error": "Username, email, and password are required"}), 400
 
-    user = Users.objects(username=username).first()
-    if user and user.check_password(password):  # Vérification du mot de passe
-        token = jwt.encode({"user_id": str(user.id)}, SECRET_KEY, algorithm="HS256")
-        return jsonify({"token": token}), 200
-    else:
-        return jsonify({"error": "Invalid credentials"}), 401
+        # Vérifier si l'utilisateur existe déjà
+        if Users.objects(username=username):
+            return jsonify({"error": "User already exists"}), 409
 
+        user = Users(username=username, email=email)
+        user.set_password(password)  # Hachage du mot de passe
+        user.save()
 
-@app.route("/auth/validate", methods=["GET"])
-def validate():
-    token = request.headers.get("Authorization")
-    if not token:
-        return jsonify({"error": "Token is missing"}), 400
-
-    try:
-        decoded = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
-        return jsonify({"status": "valid", "user_id": decoded["user_id"]}), 200
-    except jwt.ExpiredSignatureError:
-        return jsonify({"error": "Token has expired"}), 401
-    except jwt.InvalidTokenError:
-        return jsonify({"error": "Invalid token"}), 401
+        return jsonify({"message": "User created successfully"}), 201
 
 
-@app.route("/health", methods=["GET"])
-def health_check():
-    if db is None:
-        return jsonify({"auth_service": False}), 500
-    return jsonify({"auth_service": True}), 200
+    @app.route("/auth/login", methods=["POST"])
+    def login():
+        data = request.json
+        username = data.get("username")
+        password = data.get("password")
+
+        if not username or not password:
+            return jsonify({"error": "Username and password are required"}), 400
+
+        user = Users.objects(username=username).first()
+        if user and user.check_password(password):  # Vérification du mot de passe
+            token = jwt.encode({"user_id": str(user.id)}, SECRET_KEY, algorithm="HS256")
+            return jsonify({"token": token}), 200
+        else:
+            return jsonify({"error": "Invalid credentials"}), 401
+
+
+    @app.route("/auth/validate", methods=["GET"])
+    def validate():
+        token = request.headers.get("Authorization")
+        if not token:
+            return jsonify({"error": "Token is missing"}), 400
+
+        try:
+            decoded = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+            return jsonify({"status": "valid", "user_id": decoded["user_id"]}), 200
+        except jwt.ExpiredSignatureError:
+            return jsonify({"error": "Token has expired"}), 401
+        except jwt.InvalidTokenError:
+            return jsonify({"error": "Invalid token"}), 401
+
+
+    @app.route("/health", methods=["GET"])
+    def health_check():
+        if db is None:
+            return jsonify({"auth_service": False}), 500
+        return jsonify({"auth_service": True}), 200
+
+    return app
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8001)
+    get_app({
+        'db': 'authdb',
+        'host': 'mongo-auth',  # Nom du service MongoDB dans Docker
+        'port': 27017,  # Port de MongoDB
+        'username': 'root',  # Nom d'utilisateur MongoDB
+        'password': 'example',  # Mot de passe MongoDB
+        'authentication_source': 'admin'  # Base de données où les infos d'authentification sont stockées
+    }).run(host="0.0.0.0", port=8001)
--- auth_service/app/test.py
+++ auth_service/app/test.py
@@ -1,19 +1,37 @@
 # Import sys module for modifying Python's runtime environment
+import json
 import sys
 # Import os module for interacting with the operating system
 import os
+from unittest.mock import MagicMock, patch
+
+import mongomock
 
 # Add the parent directory to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 # Import pytest for writing and running tests
 import pytest
+import logging
 
 # Import the Flask app instance from the main app file
-from .main import db
-from .main import app
+from .main import Users
+from .main import get_app
+
+from mongoengine import connect, disconnect
+connection = connect('mongoenginetest', host='mongodb://localhost', alias='testdb', mongo_client_class=mongomock.MongoClient)
+
+app = get_app({
+    'db': 'testdb',
+    'host': 'localhost',  # Nom du service MongoDB dans Docker
+    'port': 27017,  # Port de MongoDB
+    'username': 'root',  # Nom d'utilisateur MongoDB
+    'password': 'example',  # Mot de passe MongoDB
+    'authentication_source': 'admin'  # Base de données où les infos d'authentification sont stockées
+})
 
 app.testing = True
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture
@@ -22,6 +40,21 @@ def client():
     with app.test_client() as client:
         yield client
 
+# @pytest.fixture
+# def mock_database():
+#     """Mock database to simulate the Users collection."""
+#     mock_users = MagicMock()
+#     mock_users.objects = MagicMock()
+#     # Simuler un comportement où `filter()` ne renvoie rien, et donc `first()` renverra None
+#     mock_users.objects.filter.return_value.first.return_value = None  # Aucun utilisateur existant
+#     return mock_users
+#
+# @pytest.fixture
+# def setup_database(mock_database):
+#     """Fixture to set up and tear down the mock database."""
+#     Users.objects = mock_database.objects
+#     yield
+#     Users.objects.delete = MagicMock()
 
 def test_health(client):
     response = client.get('/health')
@@ -29,9 +62,94 @@ def test_health(client):
     assert response.json == {'auth_service': True}
 
 
-# def test_health
+def test_signup_201(client):
+    data = {
+        "username": "newuser",
+        "email": "newuser@example.com",
+        "password": "password123"
+    }
+    response = client.post('/auth/signup', data=json.dumps(data), content_type='application/json')
+    try:
+        assert response.status_code == 201
+    finally:
+        Users.objects(username="newuser").first().delete()
+        disconnect('mongoenginetest')
 
-# def test_signup(client):
-#     response = client.post('/auth/signup', json={"username": "test", "email": "test@test.test", "password": "test"})
-#     assert response.status_code == 201
-#     assert response.json == {"message": "User created successfully"}
+
+def test_signup_409(client):
+    data = {
+        "username": "newuser",
+        "email": "newuser@example.com",
+        "password": "password123"
+    }
+
+    user = Users(username=data["username"], email=data["email"], password=data["password"])
+    user.save()
+
+    response = client.post('/auth/signup', data=json.dumps(data), content_type='application/json')
+    try:
+        assert response.status_code == 409
+    finally:
+        Users.objects(username="newuser").first().delete()
+        disconnect('mongoenginetest')
+
+
+def test_signup(client, setup_database, mock_database, capsys):
+    # Test d'inscription avec un utilisateur qui n'existe pas
+    data = {
+        "username": "newuser",
+        "email": "newuser@example.com",
+        "password": "password123"
+    }
+    logger.debug("client: %s", client)
+    print("Testing signup")
+    # Effectuer la requête POST pour l'inscription
+    response = client.post('/auth/signup', data=json.dumps(data), content_type='application/json')
+
+    captured = capsys.readouterr()
+    assert "Testing signup" in captured.out
+
+    # Afficher la réponse pour déboguer
+    print("Response data:", response.data)
+
+    # Vérification que la réponse est celle attendue pour un utilisateur créé
+    assert response.status_code == 201
+    assert response.json == {"message": "User created successfully"}
+
+    # Vérification que la méthode save() a été appelée pour enregistrer l'utilisateur
+    mock_database.objects.save.assert_called_once()  # Vérifier que save() a bien été appelée
+
+    # Simuler l'ajout de l'utilisateur et la recherche
+    mock_database.objects.filter.return_value.first.return_value = data  # L'utilisateur existe désormais
+    user = Users.objects(username="newuser").first()
+
+    assert user is not None
+    assert user.username == "newuser"
+    assert user.email == "newuser@example.com"
+
+
+
+def test_signup_user_exists(client, setup_database, mock_database):
+    # Test d'inscription avec un utilisateur qui existe déjà
+    existing_user = {
+        "username": "existinguser",
+        "email": "existinguser@example.com",
+        "password": "password123"
+    }
+
+    # Simuler qu'un utilisateur existe déjà avec le même nom d'utilisateur
+    mock_database.objects.filter.return_value.first.return_value = existing_user  # Utilisateur existant
+
+    # Données pour l'inscription
+    data = {
+        "username": "existinguser",
+        "email": "existinguser@example.com",
+        "password": "password123"
+    }
+
+    # Effectuer la requête POST pour l'inscription
+    response = client.post('/auth/signup', data=json.dumps(data), content_type='application/json')
+
+    # Vérification que la réponse est celle attendue pour un utilisateur déjà existant
+    assert response.status_code == 409
+    assert response.json == {"error": "User already exists"}
